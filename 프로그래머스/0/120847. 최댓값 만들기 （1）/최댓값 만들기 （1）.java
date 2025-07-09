a3947a1c612c8acdd58c75c4fd417709fce12164
minsung <kimminsung05@gmail.com>
2025-07-09 22:38:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -4,20 +4,36 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         
-        for(int i=0; i<numbers.length-1; i++) {
-            for(int j=i+1; j<numbers.length; j++) {
-                if(numbers[i] > numbers[j]) {                    
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];                    
-                    numbers[j] = temp;
-                }    
-            }            
-        }
+        // 1번 문제풀이 (선택정렬)
+        // for(int i=0; i<numbers.length-1; i++) {
+        //     for(int j=i+1; j<numbers.length; j++) {
+        //         if(numbers[i] > numbers[j]) {                    
+        //             int temp = numbers[i];
+        //             numbers[i] = numbers[j];                    
+        //             numbers[j] = temp;
+        //         }    
+        //     }            
+        // }
                 
-        int max1 = numbers[numbers.length-1];
-        int max2 = numbers[numbers.length-2];
+//         int max1 = numbers[numbers.length-1];
+//         int max2 = numbers[numbers.length-2];
 
-        answer = max1 * max2;
+//         answer = max1 * max2;
+        
+        // 2번 문제풀이
+        int minVal = Integer.MIN_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+         
+        for (int num : numbers) {
+            if(num > minVal) {
+                maxVal = minVal;
+                minVal = num;
+            } else if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+        
+        answer = minVal * maxVal;
 
         return answer;
     }
