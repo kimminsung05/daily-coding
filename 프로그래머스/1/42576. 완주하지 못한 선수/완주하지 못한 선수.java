e3833052ad7a1669cb6ef8e737a5e01c3374aefa
minsung <kimminsung05@gmail.com>
2025-07-22 20:02:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.43 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -16,20 +16,22 @@ class Solution {
 //         }    
         
         // 1. 해시 participant가 key count가 value
-        // 2. 
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String player : participant) 
+        // 2. 해시 completion가 key count가 value인 거를 빼서
+        // 3. value가 1인 거를 찾아 리턴
+        Map<String, Integer> map = new HashMap<>();
+        for(String player : participant)
             map.put(player, map.getOrDefault(player, 0) + 1);
         
-        for(String player : completion) 
-            map.put(player, map.get(player) - 1);        
-        
-        for(String key : map.keySet()) 
-            if(map.get(key) == 1) 
+        for(String player : completion)
+            map.put(player, map.get(player) - 1);
+                    
+        for(String key : map.keySet()) {
+            if(map.get(key) == 1) {
                 answer = key;
+                break;
+            }
+        }
             
-        
         
         return answer;
         // return participant[i];
