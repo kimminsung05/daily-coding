4ea6ef4cdf0e6df4a096da2ec2c733371697a1f2
minsung <kimminsung05@gmail.com>
2025-07-29 22:56:02 +0900

    [level 3] Title: N으로 표현, Time: 27.44 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        if (N == number) return 1;
+        
+        List<Set<Integer>> dp = new ArrayList<>();
+        
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            int repeatedNum = Integer.parseInt(String.valueOf(N).repeat(i));
+            dp.get(i).add(repeatedNum);
+            
+            for (int j = 1; j < i; j++) {
+                for (int op1 : dp.get(j)) {
+                    for (int op2 : dp.get(i - j)) {
+                        dp.get(i).add(op1 + op2);
+                        dp.get(i).add(op1 - op2);
+                        dp.get(i).add(op1 * op2);
+                        if (op2 != 0) dp.get(i).add(op1 / op2);
+                    }
+                }
+            }
+
+            if (dp.get(i).contains(number)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
